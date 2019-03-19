6d2be5f1e015be23a5017d774c039688f44b6747
Raghu Betina <raghu@firstdraft.com>
2019-03-19 08:24:40 +0000

    Only newsfeed user can access show page

--- app/controllers/newsfeeds_controller.rb
+++ app/controllers/newsfeeds_controller.rb
@@ -1,5 +1,5 @@
 class NewsfeedsController < ApplicationController
-  before_action :current_user_must_be_newsfeed_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_newsfeed_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_newsfeed_user
     newsfeed = Newsfeed.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
